cd18db067c9383fd7502397165855cac4e6e7d0a
Lars Buerger <lars.buerger@freenet.de>
2023-11-05 14:23:39 +0100

    04-CSS: Adds sparticles

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -57,8 +57,4 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def badRequest(errorMessage: String) = Action {
     BadRequest(errorMessage + "\n")
   }
-
-  def test() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.test())
-  }
 }
