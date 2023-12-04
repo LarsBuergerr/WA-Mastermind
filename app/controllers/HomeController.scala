17bae832c472135161b6b48a2cfb6d1580c44603
lars buerger <lars.buerger@freenet.de>
2023-12-04 17:35:07 +0100

    08-Websockets: Changes

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,21 +2,35 @@ package controllers
 
 import javax.inject._
 import play.twirl.api.Html
-import play.api._
-import play.api.mvc._
 import java.lang.ProcessBuilder.Redirect
 import de.htwg.se.mastermind.controller.ControllerComponent.ControllerBaseImpl._
+import de.htwg.se.mastermind.model.GameComponent.GameBaseImpl.Game
+import de.htwg.se.mastermind.model.GameComponent.GameBaseImpl.Field
 import de.htwg.se.mastermind.model.GameComponent.GameBaseImpl.{Stone, HStone, HintStone}
 import play.api.libs.json._
+import play.api.mvc._
+import akka.actor._
+import akka.stream.Materializer
+import play.api.libs.streams.ActorFlow
+import javax.inject._
+import collection.mutable.ListBuffer
+import de.htwg.se.mastermind.model.GameComponent.GameBaseImpl.Code
+import de.htwg.se.mastermind.model.FileIOComponent.fileIOjsonImpl.FileIO
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents)(implicit system: ActorSystem, mat: Materializer) extends BaseController {
 
-  var controller = new Controller()
+  var code = new Code(4)
+  var game = Game(new Field(), code, 0)
+  var controller = Controller(game, new FileIO)
+  
+  var controller_map: Map[String, Controller] = Map[String, Controller]()
+  var hash_map: Map[String, String] = Map[String, String]()
+  var client_map: Map[String, ListBuffer[ActorRef]] = Map[String, ListBuffer[ActorRef]]()
 
   def main(title: String) = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.main(title) {
@@ -28,10 +42,6 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     Ok(views.html.index(controller.gameToJson, controller.currentStoneVector, ""))
   }
 
-  def joinMultiplayer() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.joinMultiplayer())
-  }
-
   def createGame() = Action { implicit request: Request[AnyContent] =>
     print("createGame")
     controller = new Controller()
@@ -41,6 +51,49 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def displayGame() = Action { implicit request: Request[AnyContent] =>
     Ok(controller.fileIO.gameToJson(controller.game))
   }
+
+  /* ------------------- Multiplayer ------------------- */
+
+  def joinMultiplayer() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.joinMultiplayer())
+  }
+
+  def createMultiplayer() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.createMultiplayer())
+  }
+
+  def multiplayer(hash: String, player: String) = Action { implicit request: Request[AnyContent] =>
+    var multi_controller = new Controller()
+    controller_map += (hash -> multi_controller)
+    hash_map += (hash -> player)
+    Ok(views.html.multiplayer())
+  }
+
+  def join(hash: String, player: String) = Action { implicit request: Request[AnyContent] =>
+      Ok(views.html.multiplayer())
+  }
+
+  def getGameMultiplayer(hash: String) = Action { implicit request: Request[AnyContent] =>
+    print("get Game Multiplayer")
+    Ok("success")
+  }
+
+  def placeStonesMultiplayer(hash: String, stones: String) = Action { implicit request: Request[AnyContent] =>
+    val chars = stones.toCharArray()
+    val stoneVector = controller_map(hash).game.buildVector(Vector[Stone](), chars)
+    val hints = controller_map(hash).game.getCode().compareTo(stoneVector)
+
+    // Enable to see the code
+    print(controller_map(hash).game.getCode())
+    //reset currentStoneVector to only empty stones
+    controller_map(hash).currentStoneVector = Vector.from[Stone](Array.fill[Stone](controller_map(hash).game.field.matrix.cols)(Stone("E")))
+    controller_map(hash).placeGuessAndHints(stoneVector, hints, controller_map(hash).game.getCurrentTurn())
+    
+    Ok("success")
+  }
+
+
+ /* --------------------------------------------------- */
 
   /*
    * Implicit Writes for the Stone class
@@ -102,5 +155,42 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   
   def badRequest(errorMessage: String) = Action {
     BadRequest(errorMessage + "\n")
+  }
+
+  def socket(hash:String): WebSocket = WebSocket.accept[String, String] { request =>
+    ActorFlow.actorRef { out =>
+      println("Connection received")
+      MastermindSocketActorFactory.create(out,hash)
+    }
+  }
+
+  class MastermindSocketActor(out: ActorRef, hash:String) extends Actor {
+    if (client_map.contains(hash)) {
+      var a = client_map(hash)
+      a += out
+      println(a.length)
+      client_map += (hash -> a)
+    } else {
+      client_map += (hash -> ListBuffer(out))
+    }
+    def receive: Receive = {
+      case "Keep alive" => out ! "Keep alive"
+      case "refresh" => {
+        for (client <- client_map(hash)) {
+          println("refreshed")
+          println(client_map(hash).length)
+          client ! controller_map(hash).gameToJson.toString()
+        }
+      }
+      case msg: JsObject =>
+        println("ws: " + msg + " for game: " + hash)
+        out ! controller_map(hash).gameToJson.toString()
+    }
+  }
+
+  object MastermindSocketActorFactory {
+    def create(out: ActorRef, hash: String): Props = {
+      Props(new MastermindSocketActor(out, hash))
+    }
   }
 }
---
+++ public/javascripts/multiplayer.js
@@ -0,0 +1,264 @@
+let socket;
+
+window.onload = async () => {
+    await webSocketInit();
+
+    // check if player 2 when yes send start game to server
+    if(getCookie("pn") === "player2") {
+        gameChanges("/game_multiplayer/getGame/"+getCookie("game"));
+    }
+}
+
+function webSocketInit() {
+    socket = new WebSocket("ws://127.0.0.1:9000/ws/"+ getCookie("game"));
+    console.log("socket created")
+    
+    console.log("game loaded")
+    socket.onopen = () => heartBeat();
+    socket.onclose = () => console.log("Connection closed")
+    socket.onmessage = function (event) {
+        if(event.data !== "") {
+            if(event.data === "Keep alive"){
+                console.log("ping")
+            }else{
+                console.log("json reloaded");
+                console.log(JSON.parse(event.data));
+                
+                checkStatusAndUpdate(JSON.parse(event.data));
+            }
+        } else {
+            console.log("no valid json data");
+            socket.send("refresh");
+        }
+    }
+    socket.onerror = () => console.log("that was a problem")
+
+    setInterval(() => socket.send("Keep alive"), 20000); // ping every 20 seconds
+}
+
+function heartBeat() {
+    socket.send("heartBeat");
+}
+
+
+async function gameChanges(url) {
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json */*',
+            'Content-Type': 'application/json'},
+        body: ""
+    })
+    if (await res.ok) {
+        console.log("page loaded");
+        socket.send("refresh");
+    } else {
+        console.log("page failed loading");
+    }
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+
+/* ---------------------------------------------------------------------------- */
+var stoneArray = ["E", "E", "E", "E"];
+var eventListeners = []; // Store event listeners
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add event listeners after the DOM has fully loaded
+  var stoneCells = document.querySelectorAll(".stone-cell");
+  
+  stoneCells.forEach(function (element, pos) {
+    element.addEventListener("mouseover", function () {
+      startChangeStone(element, pos);
+    });
+
+    element.addEventListener("mouseout", function () {
+      stopChangeStone(element, pos);
+    });
+  });
+  // Place stones button click event
+  var placeStonesButton = document.querySelector(".placeStonesButton");
+  placeStonesButton.addEventListener("click", function () {
+      if(stoneArray.includes("E")) {
+          // Check if there are still empty stones in the array
+          createErrorPopup("Please fill all stones before placing them!");
+      } else {
+          gameChanges("/game_multiplayer/placeStones/"+getCookie("game")+"/"+stoneArray.join(""));
+      }
+  });
+});
+
+
+function createErrorPopup(message) {
+  var errorPopup = document.createElement("div");
+  errorPopup.className = "error-popup";
+  errorPopup.innerHTML = message;
+  document.body.appendChild(errorPopup);
+
+  setTimeout(function () {
+    errorPopup.style.opacity = 0;
+    setTimeout(function () {
+        errorPopup.remove();
+        }, 1000);
+    }, 3000); // 3000 milliseconds (3 seconds) until it disappears
+}
+
+function startChangeStone(element, pos) {
+  var possible_stones = ["R", "G", "B", "Y", "P", "W"];
+  var current_stone = element.src.split("_")[1].split(".")[0];
+
+    function scrollHandler(event) {
+        event.preventDefault();
+        if (event.deltaY > 0) {
+            current_stone = possible_stones[(possible_stones.indexOf(current_stone) + 1) % possible_stones.length];
+        } else {
+            current_stone = possible_stones[(possible_stones.indexOf(current_stone) - 1 + possible_stones.length) % possible_stones.length];
+        }
+        element.src = "/assets/images/stones/stone_" + current_stone + ".png";
+        stoneArray[pos] = current_stone;
+        console.log(stoneArray);
+    }
+
+    // Check if it's a touch device
+    if ('ontouchstart' in window) {
+        element.addEventListener('click', function() {
+              console.log("click triggered");
+              current_stone = possible_stones[(possible_stones.indexOf(current_stone) + 1) % possible_stones.length];
+              element.src = "/assets/images/stones/stone_" + current_stone + ".png";
+              stoneArray[pos] = current_stone;
+              console.log(stoneArray);
+        });
+    } else {
+        element.addEventListener('wheel', scrollHandler);
+    }
+    //element.addEventListener("wheel", scrollHandler);
+    //eventListeners[pos] = scrollHandler;
+}
+
+function stopChangeStone(element, pos) {
+  if (eventListeners[pos]) {
+    element.removeEventListener("wheel", eventListeners[pos]);
+  }
+}
+
+function checkStatusAndUpdate(data) {
+  console.log(data);
+  console.log(data.turn);
+  console.log(data.matrix);
+  console.log(data.hmatrix);
+  // check if every hintstone is red in the last row
+  if (data.hmatrix[data.turn].cells.every((hintstone) => hintstone.value === "R")) {  // ----- WIN GAME -----
+    $('.header-image').fadeOut('slow', function() {
+      $(this).attr('src', '/assets/images/won.png').fadeIn('slow');
+    });
+    console.log("You won!");
+    renderWinGameField(data.game)
+    // Change the function of the "Place Stone" button to start a new game
+    $('.placeStonesButton').off('click').on('click', startNewGame).text('Start New Game');
+  } else if (data.turn == data.matrix.length) {  // ----- LOSE GAME -----
+    $('.header-image').fadeOut('slow', function() {
+      $(this).attr('src', '/assets/images/loose.png').fadeIn('slow');
+    });
+    $('<link>')
+        .appendTo('head')
+        .attr({type : 'text/css', rel : 'stylesheet'})
+          .attr('href', '/assets/stylesheets/displayLoosePage.css');  
+    console.log("You lost!");
+    renderLooseGameField(data.game)
+    // Change the function of the "Place Stone" button to start a new game
+    $('.placeStonesButton').off('click').on('click', startNewGame).text('Start New Game');
+    //ameInProgress = false;
+  } else {  // ----- GAME CONTINUES -----
+    updateGameField(data);
+  }
+}
+  /**
+}
+
+/**
+ * This function is called when the "Start New Game" button is clicked
+ * which is only displayed when a game is over.
+ */
+function startNewGame() {
+  $.ajax({
+    url: '/game/createGame',
+    type: 'GET',
+    success: function(data) {
+      console.log(data);
+      currentTurn = 0;
+      updateGameField(data);
+      // Change the header image back to the original
+      $('.header-image').fadeOut('slow', function() {
+        $(this).attr('src', '/assets/images/mastermind_header_cropped.png').fadeIn('slow');
+      });
+      // Change the function of the "Place Stone" button back to place stones
+      $('.placeStonesButton').off('click').text('Place Stones');
+    }
+  });
+}
+
+function updateGameField(data) {
+
+  // Update the turn and matrix rows
+  var currentTurn = data.turn;
+
+  // Update matrix rows
+  var matrixRows = data.matrix.map(function (row) {
+    return row.cells.map(function (cell) {
+      if (row.row === currentTurn) {
+        return '<img src="/assets/images/stones/stone_A.png" class="stone-cell">';
+      } else {
+        return '<img src="/assets/images/stones/stone_' + cell.value + '.png" class="stone-cell-locked">';
+      }
+    }).join('');
+  });
+
+  // Update hint stone rows
+  var hintstoneRows = data.hmatrix.map(function (row) {
+      return row.cells.map(function (cell) {
+          return '<img src="/assets/images/hintstones/hstone_' + cell.value + '.png" class="hintstone-cell">';
+      }).join('');
+  });
+
+    // Update the game box HTML
+    $('.game-box').html('');
+    $('.game-box').append('<h3 id="demo" style="font-family: monospace; font-size: 22px; display: inline-block;">');
+
+    for (var i = 0; i < matrixRows.length; i++) {
+        if (i === currentTurn) {
+            $('.game-box h3').append('<div class="game-row">' + matrixRows[i] + '</div>');
+        } else {
+            $('.game-box h3').append('<div class="game-row">' + matrixRows[i] + '</div>');
+        }
+    }
+
+    $('.game-box').append('</h3>');
+
+    // Update the hintstone box HTML
+    $('.hintstone-box').html('');
+    $('.hintstone-box').append('<h3 style="font-family: monospace; font-size: 22px; display: inline-block;">');
+
+    for (var j = 0; j < hintstoneRows.length; j++) {
+        $('.hintstone-box h3').append('<div class="game-row">' + hintstoneRows[j] + '</div>');
+    }
+
+    $('.hintstone-box').append('</h3>');
+
+    // add event listeners to the stones
+    var stoneCells = document.querySelectorAll(".stone-cell");
+
+    stoneCells.forEach(function (element, pos) {
+        element.addEventListener("mouseover", function () {
+            startChangeStone(element, pos);
+        });
+
+        element.addEventListener("mouseout", function () {
+            stopChangeStone(element, pos);
+        });
+    });
+}
